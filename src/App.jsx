7e1d87d4456bb28a56c1c353879b22c97f22197b
krjofficial <krishrjain.official@gmail.com>
2024-12-27 09:52:52 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
-import Card from "./components/card";
 import HeroSection from "./components/HeroSection";
+import Card from "./components/card";
+import About from "./pages/About";
+import Achievements from "./pages/Achievements";
+import Projects from "./pages/Projects";
+import TheTimeline from "./pages/TheTimeline";
+import Experience from "./pages/Experience";
 
-function App(props) {
+function App() {
   return (
-    <>
-      <div className="m-9">
-        <HeroSection />
-      </div>
-      <div className="flex space-x-8">
-        <Card title="Who am I" />
-        <Card title="Projects" />
-        <Card title="The Timeline" />
-        <Card title="Achievements" />
-        <Card title="Experience" />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Root Route: Render HeroSection and Cards */}
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="m-9">
+                <HeroSection />
+              </div>
+              <div className="flex space-x-8">
+                <Card title="Who am I" link="/about" />
+                <Card title="Projects" link="/projects" />
+                <Card title="The Timeline" link="/timeline" />
+                <Card title="Achievements" link="/achievements" />
+                <Card title="Experience" link="/experience" />
+              </div>
+            </div>
+          }
+        />
+
+        {/* Individual Pages */}
+        <Route path="/about" element={<About />} />
+        <Route path="/achievements" element={<Achievements />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/timeline" element={<TheTimeline />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/TheTimeline.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TheTimeline() {
+  return (
+    <div>
+      The Timeline
+    </div>
+  )
+}
