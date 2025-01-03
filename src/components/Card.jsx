effb50733b135058c52e6c44fbd3e152cb11a09c
krjofficial <krishrjain.official@gmail.com>
2025-01-03 11:22:08 +0530

    styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { Link } from "react-router";
 
 function Card(props) {
   return (
-    <div className="card bg-base-100 w-100 shadow-xl">
-    <div className="card-body">
-      <h2 className="card-title">{props.title}</h2>
-      <div className="card-actions justify-end">
-        <button className="btn btn-primary">Click</button>
+    <Link
+      to={props.link || "/"}
+      className="no-underline w-full text-white" // Added w-full for full width if needed
+    >
+      <div className="card-bordered m-5 border-yellow-500 bg-[#242424] text-white/87 w-full shadow-lg shadow-yellow-600 
+                      hover:bg-[#333] hover:text-white hover:border-orange-300 hover:scale-105 hover:shadow-lg hover:shadow-orange-700 transition-all duration-300
+                      flex flex-col justify-center items-center text-center p-4 rounded-xl" // Added Tailwind classes
+      >
+        <div className="card-body">
+          <h2 className="card-title font-bold">{props.title}</h2>
+        </div>
       </div>
-    </div>
-  </div>
+    </Link>
   );
 }
 
